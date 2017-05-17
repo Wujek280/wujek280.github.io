a3735e577bca2cbb6d6df5a1f9886af5efc452f3
Wujek280 <mjwojcik94@gmail.com>
2017-05-17 22:38:22 +0200

    test

--- js/popup.js
+++ js/popup.js
@@ -26,31 +26,29 @@ function popUp(){
 
 	var bgDiv = document.createElement('div');
 	bgDiv.className = 'backgroundDiv';
+	bgDiv.innerHTML = " <div class='popUpMenu'> <div class='callDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Cześć</h2> <p>Zawsze możesz do nas zadzwonić</p><h3>+48 664 340 449</h3> <i class='phone fa fa-phone-square fa-5x'></i> </div><div class='replyDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Zostaw nam wiadomość</h2> <hr> <form> <button class='button-clear' onclick='formClear();' >Wyczyść</button> <button class='button-send' >Wyślij</button> <label for='name'>Imię</label> <br><input id='name' name='name' type='text' > <br><label for='surname'>Nazwisko</label> <br><input id='surname' name='surname' type='text' > <br><label for='phone'>Telefon</label> <br><input id='phone' name='phone' type='tel' placeholder='790 654 321...'><br><label for='message' >Wiadomość</label> <br><textarea id='message' name='message' placeholder='Napisz nam coś...'> </textarea> </form> </div></div>"
 	document.getElementsByTagName('nav')[0].appendChild(bgDiv);
 
-	var popUpMenuDiv = document.createElement('div');
-	popUpMenuDiv.className = 'popUpMenu';
-	document.getElementsByClassName('backgroundDiv')[0].appendChild(popUpMenuDiv);
+	// var popUpMenuDiv = document.createElement('div');
+	// popUpMenuDiv.className = 'popUpMenu';
+	// document.getElementsByClassName('backgroundDiv')[0].appendChild(popUpMenuDiv);
 
-	var callDiv = document.createElement('div');
-	callDiv.className = 'callDiv';          
-	callDiv.innerHTML= '<h2>Hello</h2><p>Lorme ipsum</p><i class="fa fa-telephone fa-2x"></i>';
-	document.getElementsByClassName('popUpMenu')[0].appendChild(callDiv);
+	// var callDiv = document.createElement('div');
+	// callDiv.className = 'callDiv';          
+	// callDiv
+	// document.getElementsByClassName('popUpMenu')[0].appendChild(callDiv);
 
-	var replyDiv = document.createElement('div');
-	replyDiv.className = 'replyDiv';
-	document.getElementsByClassName('popUpMenu')[0].appendChild(replyDiv);
-
-	document.getElementsByClassName('backgroundDiv')[0].onclick = function(){
-			closePopUp();
-	}
+	// var replyDiv = document.createElement('div');
+	// replyDiv.className = 'replyDiv';
+	// document.getElementsByClassName('popUpMenu')[0].appendChild(replyDiv);
 
 	console.log('popup working');
 }
 
 function closePopUp(){
+	console.log('closing triggered');
 	// document.body.className = '';
-	// document.getElementsByClassName('backgroundDiv')[0].outerHTML = '';
+	document.getElementsByClassName('backgroundDiv')[0].outerHTML = '';
 
 }
 
@@ -65,6 +63,5 @@ function formClear(){
 	}
 
 	document.getElementsByTagName('textarea')[0].value = '';
-
 
 }
