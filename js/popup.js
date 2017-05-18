85cc0cfeefcd82bda66c8228e3773b95d34ba218
Wujek280 <mjwojcik94@gmail.com>
2017-05-18 17:09:38 +0200

    nofollow, button to anchor change

--- js/popup.js
+++ js/popup.js
@@ -29,19 +29,6 @@ function popUp(){
 	bgDiv.innerHTML = " <div class='popUpMenu'> <div class='callDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Cześć</h2> <p>Zawsze możesz do nas zadzwonić</p><h3>+48 664 340 449</h3> <i class='phone fa fa-phone-square fa-5x'></i> </div><div class='replyDiv'> <i class='times fa fa-times fa-2x' onclick='closePopUp();'></i> <h2>Zostaw nam wiadomość</h2> <hr> <form> <button class='button-clear' onclick='formClear();' >Wyczyść</button> <button class='button-send' >Wyślij</button> <label for='name'>Imię</label> <br><input id='name' name='name' type='text' > <br><label for='surname'>Nazwisko</label> <br><input id='surname' name='surname' type='text' > <br><label for='phone'>Telefon</label> <br><input id='phone' name='phone' type='tel' placeholder='790 654 321...'><br><label for='message' >Wiadomość</label> <br><textarea id='message' name='message' placeholder='Napisz nam coś...'> </textarea> </form> </div></div>"
 	document.getElementsByTagName('nav')[0].appendChild(bgDiv);
 
-	// var popUpMenuDiv = document.createElement('div');
-	// popUpMenuDiv.className = 'popUpMenu';
-	// document.getElementsByClassName('backgroundDiv')[0].appendChild(popUpMenuDiv);
-
-	// var callDiv = document.createElement('div');
-	// callDiv.className = 'callDiv';          
-	// callDiv
-	// document.getElementsByClassName('popUpMenu')[0].appendChild(callDiv);
-
-	// var replyDiv = document.createElement('div');
-	// replyDiv.className = 'replyDiv';
-	// document.getElementsByClassName('popUpMenu')[0].appendChild(replyDiv);
-
 	console.log('popup working');
 }
 
